779544aed128a0df2aa51bf91313c118f70c380a
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-20 20:22:20 +0330

    Implement Project object

---
+++ task-management/Entities/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace task_management.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+
+
+
+    }
+}
