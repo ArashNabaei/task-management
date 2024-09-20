245305656eab41e47118503f08aa628cf27b65cf
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-20 20:29:45 +0330

    Create Task object

---
+++ task-management/Entities/Task.cs
@@ -0,0 +1,21 @@
+﻿namespace task_management.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Status Status { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int LoggedHours { get; set; }
+    }
+}
