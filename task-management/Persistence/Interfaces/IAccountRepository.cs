87444de192c454b6373403a3358c4adad592770e
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-22 12:40:28 +0330

    Add create user and get user methods to IAccountRepository

--- task-management/Persistence/Interfaces/IAccountRepository.cs
+++ task-management/Persistence/Interfaces/IAccountRepository.cs
@@ -2,5 +2,8 @@
 {
     public interface IAccountRepository
     {
+        Task CreateUser(string username, string password);
+
+        Task<Entities.User?> GetUser(string username, string password);
     }
 }
