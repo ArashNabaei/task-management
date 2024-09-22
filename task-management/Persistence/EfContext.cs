2d1ea089cd5bf13602b3f02b0085609e6037ae8f
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-22 12:05:11 +0330

    Add tables to EfContext class

--- task-management/Persistence/EfContext.cs
+++ task-management/Persistence/EfContext.cs
@@ -1,6 +1,22 @@
-﻿namespace task_management.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using task_management.Entities;
+
+namespace task_management.Persistence
 {
-    public class EfContext
+    public class EfContext : DbContext
     {
+
+        public EfContext(DbContextOptions<EfContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+        
+        public DbSet<Entities.Task> Tasks { get; set; }
+        
+        public DbSet<SubTask> SubTasks { get; set; }
+
     }
 }
