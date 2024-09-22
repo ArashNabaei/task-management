7a52f8e6b56187063894cbdfbdbb4d0499d1b9d2
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-22 12:59:28 +0330

    Make a new migration for renaming Task to WorkTask

---
+++ task-management/Migrations/20240922092830_RenameTaskToWorkTask.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace task_management.Migrations
+{
+    public partial class RenameTaskToWorkTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
