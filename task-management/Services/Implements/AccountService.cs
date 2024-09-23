7a6945b86a70cce8bb23d946f1db8a197f007bfe
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-23 23:50:14 +0330

    Create AccountService Implement CreateUser method in AccountService

---
+++ task-management/Services/Implements/AccountService.cs
@@ -0,0 +1,31 @@
+﻿using task_management.Entities;
+using task_management.Persistence.Interfaces;
+using task_management.Services.Interfaces;
+
+namespace task_management.Services.Implements
+{
+    public class AccountService : IAccountService
+    {
+
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task CreateUser(Account user)
+        {
+            var username = user.Username;
+            var password = user.Password;
+
+            var existingUser = await _accountRepository.GetUser(username, password);
+
+            if (existingUser != null)
+                throw new Exception("User already exists.");
+
+            await _accountRepository.CreateUser(username, password);
+        }
+
+    }
+}
