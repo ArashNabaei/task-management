fc436efa9412b5adab30e0dbde4d99bf8c91f622
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-24 11:53:37 +0330

    Create new controller for profiles Implement GetProfile API

---
+++ task-management/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using task_management.Entities;
+using task_management.Services.Interfaces;
+
+namespace task_management.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProfileController : BaseController
+    {
+        private readonly IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [HttpGet("Profile")]
+        public async Task<IActionResult> GetProfile()
+        {
+            var profile = await _profileService.GetProfile(UserId);
+
+            return Ok(new { Profile = profile });
+        }
+
+    }
+}
