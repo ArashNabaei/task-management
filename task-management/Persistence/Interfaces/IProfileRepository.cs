91e3f617fab413fd662a3b32673d38349315c89b
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-24 11:23:04 +0330

    Create IProfileRepository interface

---
+++ task-management/Persistence/Interfaces/IProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using task_management.Entities;
+
+namespace task_management.Persistence.Interfaces
+{
+    public interface IProfileRepository
+    {
+
+        Task<User?> GetProfile(int UserId);
+
+        Task UpdateProfile(int userId, User user);
+    }
+}
