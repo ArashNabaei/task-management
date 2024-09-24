4598cfda66e5a80798d4b004259765244adbe910
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-24 11:43:39 +0330

    Create IProfileService file

---
+++ task-management/Services/Interfaces/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using task_management.Entities;
+
+namespace task_management.Services.Interfaces
+{
+    public interface IProfileService
+    {
+        Task<User?> GetUser(int id);
+
+        Task UpdateUser(int id, User user);
+    }
+}
