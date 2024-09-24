5daf25c8fa658dce5a7b36b8b4db87bd6a1dae4f
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-24 11:11:32 +0330

    Make fields of entities nullable

--- task-management/Entities/Project.cs
+++ task-management/Entities/Project.cs
@@ -4,20 +4,20 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
         public int UserId { get; set; }
 
         public User User { get; set; }
 
-        public Status Status { get; set; }
+        public Status? Status { get; set; }
 
-        public Priority Priority { get; set; }
+        public Priority? Priority { get; set; }
 
-        public List<WorkTask> Tasks { get; set; }
+        public List<WorkTask>? Tasks { get; set; }
     }
 }
--- task-management/Entities/WorkTask.cs
+++ task-management/Entities/WorkTask.cs
@@ -4,22 +4,22 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
-        public Status Status { get; set; }
+        public Status? Status { get; set; }
 
-        public Priority Priority { get; set; }
+        public Priority? Priority { get; set; }
 
         public int ProjectId { get; set; }
 
-        public Project Project { get; set; }
+        public Project? Project { get; set; }
 
         public int LoggedHours { get; set; }
 
-        public List<SubTask> SubTasks { get; set; }
+        public List<SubTask>? SubTasks { get; set; }
     }
 }
