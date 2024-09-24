5a2dcea3fd00dddb8b5ae9f84802a7e4e63d6499
ArashNabaei <Arash_ANB@yahoo.com>
2024-09-24 11:48:06 +0330

    Create ProfileService class Implement GetProfile method in ProfileService

---
+++ task-management/Services/Implements/ProfileService.cs
@@ -0,0 +1,25 @@
+﻿using task_management.Entities;
+using task_management.Persistence;
+using task_management.Persistence.Interfaces;
+using task_management.Services.Interfaces;
+
+namespace task_management.Services.Implements
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileService(IProfileRepository profileRepository)
+        {
+            _profileRepository = profileRepository;
+        }
+
+        public async Task<User?> GetProfile(int id)
+        {
+            var user = await _profileRepository.GetProfile(id);
+
+            return user;
+        }
+
+    }
+}
--- task-management/Services/Interfaces/IProfileService.cs
+++ task-management/Services/Interfaces/IProfileService.cs
@@ -4,7 +4,7 @@ namespace task_management.Services.Interfaces
 {
     public interface IProfileService
     {
-        Task<User?> GetUser(int id);
+        Task<User?> GetProfile(int id);
 
         Task UpdateUser(int id, User user);
     }
